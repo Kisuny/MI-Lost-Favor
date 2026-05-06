05067a7006b3a93f6883a4f85dd24e2b5b2cd3d4
Kisuny <kitsunyme@gmail.com>
2026-05-06 22:38:34 +0300

    doTraderSpawning false -> true, playersSleepingPercentage 20 -> 40

--- kubejs/server_scripts/gamerules.js
+++ kubejs/server_scripts/gamerules.js
@@ -3,11 +3,10 @@ ServerEvents.loaded(event => {
     if (!persistentData.firstLoad) {
         persistentData.firstLoad = true
 
-        server.runCommandSilent('gamerule doTraderSpawning false')
         server.runCommandSilent('gamerule doInsomnia false')
         server.runCommandSilent('gamerule coinDropPercent 0')
         server.runCommandSilent('gamerule doPatrolSpawning false')
-        server.runCommandSilent('gamerule playersSleepingPercentage 20')
+        server.runCommandSilent('gamerule playersSleepingPercentage 40')
         server.runCommandSilent('gamerule doFireTick false')
     }
 })

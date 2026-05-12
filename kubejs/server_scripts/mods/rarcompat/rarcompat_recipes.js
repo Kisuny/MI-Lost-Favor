048843c233e61ed1a1a69a54456a16c3d216fbaf
Kisuny <kitsunyme@gmail.com>
2026-05-12 16:10:37 +0300

    New recipe for mimi_dust

---
+++ kubejs/server_scripts/mods/rarcompat/rarcompat_recipes.js
@@ -0,0 +1,14 @@
+ServerEvents.recipes(event => {
+
+    miMachineCraft(event, {energy:2, time:600, machine:"modern_industrialization:mixer",
+        inputItems:[
+            [{item:"eidolon_repraised:soul_shard"}, 2],
+            [{item:"occultism:echo_dust"}],
+        ],
+        outputItems:[
+            [{item:"rarcompat:mimi_dust"}]
+        ],
+        removeRecipe: true
+    })
+
+})

ea840cf6bd1e5038e8230e95adebef9b30f25f3e
Kisuny <kitsunyme@gmail.com>
2026-05-21 06:31:05 +0300

    Chunk Loaders mod has been fully integrated into the modpack

--- kubejs/server_scripts/mods/chunk_loaders/chunk_loaders_recipes.js
+++ kubejs/server_scripts/mods/chunk_loaders/chunk_loaders_recipes.js
@@ -1,9 +1,45 @@
 ServerEvents.recipes(event => {
-    
-    event.remove({output: [
-        "chunkloaders:basic_chunk_loader",
-        "chunkloaders:advanced_chunk_loader",
-        "chunkloaders:ultimate_chunk_loader"
-    ]})
+    milfShaped(event, {
+        pattern: [
+            "wew",
+            "eqe",
+            "wew"
+        ],
+        key: {
+            q: { item: "modern_industrialization:analog_circuit" },
+            w: { item: "enchanted:attuned_stone_charged" },
+            e: { item: "immersiveengineering:heavy_engineering" }
+        },
+        outputItems: [[{ id: "chunkloaders:basic_chunk_loader" }]],
+        removeRecipe: true
+    })
+    milfShaped(event, {
+        pattern: [
+            "wew",
+            "eqe",
+            "wew"
+        ],
+        key: {
+            q: { item: "chunkloaders:basic_chunk_loader" },
+            w: { item: "modern_industrialization:advanced_machine_hull" },
+            e: { item: "modern_industrialization:electronic_circuit" }
+        },
+        outputItems: [[{ id: "chunkloaders:advanced_chunk_loader" }]],
+        removeRecipe: true
+    })
+    milfShaped(event, {
+        pattern: [
+            "wew",
+            "eqe",
+            "wew"
+        ],
+        key: {
+            q: { item: "chunkloaders:advanced_chunk_loader" },
+            w: { item: "modern_industrialization:quantum_machine_hull" },
+            e: { item: "modern_industrialization:quantum_circuit" }
+        },
+        outputItems: [[{ id: "chunkloaders:ultimate_chunk_loader" }]],
+        removeRecipe: true
+    })
     
 })

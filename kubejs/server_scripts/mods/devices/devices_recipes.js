1c768458b267ed27c12d768571b180641be53e54
Kisuny <kitsunyme@gmail.com>
2026-05-22 06:29:13 +0300

    Revert "JsonIO -> generateData"
    This reverts commit 8657aa9880ade003f8da77a4dcaf75985759bf71.

--- kubejs/server_scripts/mods/devices/devices_recipes.js
+++ kubejs/server_scripts/mods/devices/devices_recipes.js
@@ -1,7 +1,18 @@
 ServerEvents.recipes(event => {
     
     event.remove({output: "devices:magical_pouch"})
-
+    addHephaestusRitual("magical_pouch", {
+        enhancers: "forbidden_arcanus:crimson_stone",
+        essences: { aureal: 50, blood: 500, souls: 20, experience: 220 },
+        mainIngredient: "devices:devices_pouch",
+        inputs: [
+            { item: "minecraft:nether_star", amount: 1 },
+            { item: "hexerei:infused_fabric", amount: 2 },
+            { item: "modern_industrialization:aluminum_plate", amount: 2 },
+        ],
+        result: "devices:magical_pouch",
+    })
+    
     customMixingCauldron(event, {
         fluid: "minecraft:water",
         fluidAmount: 1000,
--- kubejs/server_scripts/mods/tempad/tempad_recipes.js
+++ kubejs/server_scripts/mods/tempad/tempad_recipes.js
@@ -12,4 +12,15 @@ ServerEvents.recipes(event => {
     event.replaceInput({ id: "tempad:timedoor_projector" }, "minecraft:copper_ingot", "modern_industrialization:steel_ingot")
 
     event.remove({ output: "tempad:time_steel" })
+    addHephaestusRitual("tempad_time_steel", {
+        enhancers: "forbidden_arcanus:elementarium",
+        essences: { },
+        mainIngredient: "minecraft:iron_ingot",
+        inputs: [
+            { item: "modern_industrialization:stainless_steel_ingot", amount: 2 },
+            { item: "minecraft:netherite_ingot", amount: 2 },
+        ],
+        resultCount: 2,
+        result: "tempad:time_steel",
+    })
 }) 

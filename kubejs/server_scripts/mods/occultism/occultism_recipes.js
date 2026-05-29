d58ef104af0a51816b9b21d9f0d0906b9a843f54
Kisuny <kitsunyme@gmail.com>
2026-05-29 11:17:57 +0300

    new item in occultism chalks

--- kubejs/server_scripts/mods/occultism/occultism_recipes.js
+++ kubejs/server_scripts/mods/occultism/occultism_recipes.js
@@ -1,12 +1,14 @@
 ServerEvents.recipes(event => {
-    
-    event.remove({output: [
-        "occultism:raw_silver_block",
-        "occultism:silver_ingot",
-        "occultism:silver_block",
-        "occultism:silver_nugget",
-        "occultism:raw_silver",
-    ]})
+
+    event.remove({
+        output: [
+            "occultism:raw_silver_block",
+            "occultism:silver_ingot",
+            "occultism:silver_block",
+            "occultism:silver_nugget",
+            "occultism:raw_silver",
+        ]
+    })
     event.replaceOutput({ output: 'occultism:netherite_dust' }, 'occultism:netherite_dust', 'modern_industrialization:netherite_dust')
     customAlchemicalForgeCraft(event, {
         affinities: [
@@ -23,11 +25,43 @@ ServerEvents.recipes(event => {
             }
         ],
         main: {
-            "tag" : "c:seeds"
+            "tag": "c:seeds"
         },
         result: "occultism:datura_seeds"
     })
-    
+
+
+    event.replaceInput({ output: "occultism:spirit_attuned_gem" }, "occultism:spirit_attuned_gem", "eidolon_repraised:soul_shard")
+
+    const addAdditionItemInShapelessPairs = [
+        ["occultism:chalk_yellow_impure", "modern_industrialization:bronze_dust"],
+        ["occultism:chalk_orange_impure", "modern_industrialization:bronze_dust"],
+        ["occultism:chalk_purple_impure", "modern_industrialization:battery_alloy_dust"],
+        ["occultism:chalk_light_blue_impure", "modern_industrialization:aluminum_dust"],
+        ["occultism:chalk_red_impure", "modern_industrialization:ruby_dust"],
+        ["occultism:chalk_black_impure", "modern_industrialization:steel_dust"],
+        ["occultism:chalk_magenta_impure", "modern_industrialization:titanium_dust"],
+        ["occultism:chalk_cyan_impure", "modern_industrialization:celestigem_dust"],
+    ]
+
+
+    addAdditionItemInShapelessPairs.forEach(([output, newItem]) => {
+        event.forEachRecipe({ output: output }, recipe => {
+
+            const rJSON = JSON.parse(recipe.json)
+            const ingredients = rJSON.ingredients
+            const result = rJSON.result
+
+            ingredients.push({ item: newItem })
+            const inputItems = ingredients.map(ingredient => [ingredient])
+
+            milfShapeless(event, {
+                inputItems: inputItems,
+                outputItems: [[{ id: result.id }]],
+                removeRecipe: true
+            })
+        })
+    })
 })
 
 

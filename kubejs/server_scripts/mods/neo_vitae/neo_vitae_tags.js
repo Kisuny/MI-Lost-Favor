88b6f19e7869899f946ca7e4b4f9db1dcfe12315
Kisuny <kitsunyme@gmail.com>
2026-06-10 10:58:54 +0300

    All Neo Vitae content has been added to the quests

--- kubejs/server_scripts/mods/neo_vitae/neo_vitae_tags.js
+++ kubejs/server_scripts/mods/neo_vitae/neo_vitae_tags.js
@@ -1,4 +1,8 @@
 ServerEvents.tags('item', event => {
 
+    event.add("neovitae:altar/runes_t1", ['neovitae:rune_blank', 'neovitae:rune_sacrifice', 'neovitae:rune_sacrifice_self', 'neovitae:rune_speed'])
+    event.add("neovitae:altar/runes_t2", ['neovitae:rune_blank', 'neovitae:rune_sacrifice', 'neovitae:rune_sacrifice_self', 'neovitae:rune_speed', 'neovitae:rune_dislocation', 'neovitae:rune_capacity'])
+    event.add("neovitae:altar/runes_t3", ['neovitae:rune_blank', 'neovitae:rune_sacrifice', 'neovitae:rune_sacrifice_self', 'neovitae:rune_speed', 'neovitae:rune_dislocation', 'neovitae:rune_capacity', 'neovitae:rune_capacity_augmented', 'neovitae:rune_acceleration', 'neovitae:rune_charging', 'neovitae:rune_orb', 'neovitae:rune_capacity_augmented', 'neovitae:rune_orb', 'neovitae:rune_charging', 'neovitae:rune_acceleration'])
+    event.add("neovitae:altar/runes_t4", ['neovitae:rune_blank', 'neovitae:rune_sacrifice', 'neovitae:rune_sacrifice_self', 'neovitae:rune_speed', 'neovitae:rune_dislocation', 'neovitae:rune_capacity', 'neovitae:rune_capacity_augmented', 'neovitae:rune_acceleration', 'neovitae:rune_charging', 'neovitae:rune_orb', 'neovitae:rune_capacity_augmented', 'neovitae:rune_orb', 'neovitae:rune_charging', 'neovitae:rune_acceleration', 'neovitae:rune_efficiency', 'neovitae:rune_2_sacrifice', 'neovitae:rune_2_sacrifice_self', 'neovitae:rune_2_capacity', 'neovitae:rune_2_capacity_augmented', 'neovitae:rune_2_charging', 'neovitae:rune_2_speed', 'neovitae:rune_2_acceleration', 'neovitae:rune_2_dislocation', 'neovitae:rune_2_orb', 'neovitae:rune_2_efficiency'])
 
 })

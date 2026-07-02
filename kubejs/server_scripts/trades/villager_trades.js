0382b412334e05f7ed7fc13f9da3e95ca1fddf8d
Kisuny <kitsunyme@gmail.com>
2026-07-02 13:22:15 +0300

    Remove MI, AE2, IE villager trades

---
+++ kubejs/server_scripts/trades/villager_trades.js
@@ -0,0 +1,12 @@
+MoreJS.villagerTrades(event => {
+    event.removeModdedTypedTrades([
+        "immersiveengineering:outfitter",
+        "immersiveengineering:gunsmith",
+        "immersiveengineering:electrician",
+        "immersiveengineering:machinist",
+        "immersiveengineering:engineer",
+        "ae2:fluix_researcher" 
+    ], [1, 5]
+    )
+
+})

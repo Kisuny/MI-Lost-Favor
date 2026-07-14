d8e82b85752b65e42fe4987e33f70e1aa4900e55
Kisuny <kitsunyme@gmail.com>
2026-07-14 18:18:58 +0300

    Added info how to get cursed items

--- kubejs/server_scripts/EMI/info.js
+++ kubejs/server_scripts/EMI/info.js
@@ -2,4 +2,7 @@ RecipeViewerEvents.addInformation('item', event => {
 	event.add('immersiveengineering:concrete', [
 		Text.translate('milf.emi_info.concrete')
 	])
+	event.add(['enigmaticlegacyplus:evil_essence', 'enigmaticlegacyplus:abyssal_heart', 'milf:soul_of_the_helpless'], [
+		Text.translate('milf.cursed_drops')
+	])
 })

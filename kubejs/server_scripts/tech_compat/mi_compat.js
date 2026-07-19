d8c05519c36c67ab016a8542983eecaee66dbdc0
saeta <saeta_@outlook.com>
2026-07-20 01:05:55 +0700

    tech_compat WIP

--- kubejs/server_scripts/tech_compat/mi_compat.js
+++ kubejs/server_scripts/tech_compat/mi_compat.js
@@ -1,5 +1,5 @@
 ServerEvents.tags('item', event => {
-    var remove = [
+    let noCompat = [
         //item outputs that will not be converted
         /.*fine_wire/,
         "modern_industrialization:diamond_tiny_dust",
@@ -9,34 +9,90 @@ ServerEvents.tags('item', event => {
         "modern_industrialization:fire_clay_bricks",
     ]
 
-    remove.forEach(output => {
-        event.add('milf:nocompat', output)
-    })
+    event.add('milf:nocompat', noCompat)
 })
 
 ServerEvents.recipes(event => {
     //macerator to crusher
-    function crusher_mi(ingredients, result, miEnergy){
-        let output = []
-        result.forEach((out) => output.push([{item: out.item}, out.amount, out.probability]))
-        ieCrusherCraft(event, {
-            inputItems: ingredients.tag ? [[{tag:ingredients.tag}, ingredients.amount]] : [[{item:ingredients.item}, ingredients.amount]],
-            outputItems: output,
-            energy: miEnergy,
-            compatOff: true,
-            removeRecipeType:"immersiveengineering:crusher"
+    function crusherAndPulverizer(ingredient, result, miEnergy){
+        let ieOutput = []
+        let oriOutput = []
+        result.forEach((out) => {
+            ieOutput.push([{ item: out.item }, out.amount, out.probability])
+            oriOutput.push([{ id: out.item }, out.amount, out.probability])
         })
+
+        let item
+        let isRawOre = false
+        let isOre = false
+
+        if (ingredient.item){            
+            item = Item.of(ingredient.item)
+            if (item && (item.hasTag("c:raw_materials")) || (item.hasTag("c:crushed_ores"))) {
+                isRawOre = true
+            }
+            if (item && item.hasTag("c:ores")) {
+                isOre = true
+            }
+        } else {
+            let key = $TagKey.create($Registries.ITEM, $ResourceLocation.parse(ingredient.tag))
+            let tagItems = $BuiltInRegistries.ITEM.getOrCreateTag(key)
+            isRawOre = tagItems.stream().anyMatch(holder => {
+                let currentItem = Item.of(holder.value())
+                return currentItem && (currentItem.hasTag("c:raw_materials")) || (currentItem.hasTag("c:crushed_ores"))
+            })
+            isOre = tagItems.stream().anyMatch(holder => {
+                let currentItem = Item.of(holder.value())
+                return currentItem && currentItem.hasTag("c:ores")
+            })
+        }
+
+        if(isOre){
+            ieCrusherCraft(event, {
+                inputItems: ingredient.tag ? [[{ tag: ingredient.tag }, ingredient.amount]] : [[{ item: ingredient.item }, ingredient.amount]],
+                outputItems: ieOutput.map(entry => [entry[0], Math.ceil(entry[1] * 1.5), entry[2]]),
+                energy: miEnergy,
+                compatOff: true,
+                removeRecipeType: "immersiveengineering:crusher"
+            })
+        } else {
+            ieCrusherCraft(event, {
+                inputItems: ingredient.tag ? [[{ tag: ingredient.tag }, ingredient.amount]] : [[{ item: ingredient.item }, ingredient.amount]],
+                outputItems: ieOutput,
+                energy: miEnergy,
+                compatOff: true,
+                removeRecipeType: "immersiveengineering:crusher"
+            })
+        }
+
+
+
+        if (isRawOre && oriOutput.length <= 2){
+            //console.log(ingredient);
+            
+            oritechPulverizerRecipe(event, {
+                inputItems: ingredient.tag ? [[{ tag: ingredient.tag }, ingredient.amount]] : [[{ item: ingredient.item }, ingredient.amount]],
+                outputItems: oriOutput,
+                compatOff: true,
+
+            })
+        }        
+
         //event.remove({ type: 'immersiveengineering:crusher', output:result[0].item})
 
     }
 
     event.remove({ type: 'immersiveengineering:crusher', output:"minecraft:amethyst_shard"})
+    event.remove({ type: 'immersiveengineering:crusher', output: "minecraft:emerald" })
+    event.remove({ type: 'immersiveengineering:crusher', output: "minecraft:coal" })
+    event.remove({ type: 'immersiveengineering:crusher', output: "minecraft:diamond" })
 
     event.forEachRecipe({ type: 'modern_industrialization:macerator', not : {output: "#milf:nocompat"}}, r => {
         const rjson = JSON.parse(r.json)
         if(!Array.isArray(rjson.item_inputs)){rjson.item_inputs = [rjson.item_inputs]}
         if(!Array.isArray(rjson.item_outputs)){rjson.item_outputs = [rjson.item_outputs]}
-        crusher_mi((rjson.item_inputs)[0], rjson.item_outputs, rjson.duration * rjson.eu)
+        crusherAndPulverizer((rjson.item_inputs)[0], rjson.item_outputs, rjson.duration * rjson.eu * 10)
+
         rjson.item_outputs.forEach(output =>{            
             event.remove({ type: 'immersiveengineering:crusher', output:output?.item || `#${output?.tag}`})
         })

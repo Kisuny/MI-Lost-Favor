f1d2466c675e2dfcfdde7907e73a4dccd7104da5
saeta <saeta_@outlook.com>
2026-07-19 17:39:32 +0700

    fluids

--- kubejs/startup_scripts/registration/fluids.js
+++ kubejs/startup_scripts/registration/fluids.js
@@ -1,9 +1,17 @@
 createNewFluid("liquid_plastic", {textureType:"thick", color: Number("0xFFFFFF"), lang: { "en_us": "Liquid Plastic", "ru_ru": "Жидкий пластик" }})
+createNewFluid("silicone_modified_phenolic_resin", { textureType: "thick", color: Number("0x778751"), lang: { "en_us": "Silicone-modified Phenolic Resin" } })
 createNewFluid("desalted_crude_oil", {textureType:"thick", color: Number("0x292520"), lang: { "en_us": "Desalted Crude Oil", "ru_ru": "Обессоленная нефть" }})
 createNewFluid("high_sulfur_kerosene", {textureType:"thin", color: Number("0x735b3e"), lang: { "en_us": "High Sulfur Kerosene", "ru_ru": "Высокосернистый керосин" }})
+
+
 createNewFluid("ethereal_source", {
     textureType:"thin",
     stillTexture: "milf:fluid/ethereal_source",
     flowingTexture: "milf:fluid/ethereal_source",
     lang: { "en_us": "Ethereal Source", "ru_ru": "Эфирный источник" }
+})
+
+createNewFluid("alien_goo", {
+    stillTexture: "milf:fluid/alien_goo",
+    flowingTexture: "milf:fluid/alien_goo",
 })

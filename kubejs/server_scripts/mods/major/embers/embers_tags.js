3a84a22521c04591b4c262ec2649203e351d3148
Kisuny <kitsunyme@gmail.com>
2026-07-27 07:47:53 +0300

    Added all items from embers into quests [WIP text]

---
+++ kubejs/server_scripts/mods/major/embers/embers_tags.js
@@ -0,0 +1,10 @@
+ServerEvents.tags('item', event => {
+    
+    event.add("embers:reservoir_expansion", [
+        'embers:caminite_ring', 
+        'embers:caminite_gauge', 
+        'embers:caminite_valve',
+    ])
+    
+})
+

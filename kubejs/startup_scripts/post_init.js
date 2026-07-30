ebb2273eeddcb549544d9603f00eca52e1d32f4e
saeta <saeta_@outlook.com>
2026-07-30 22:22:55 +0700

    cursed stuff

---
+++ kubejs/startup_scripts/post_init.js
@@ -0,0 +1,9 @@
+// StartupEvents.postInit(event => {
+//     let blockEntityType = $BuiltInRegistries.BLOCK_ENTITY_TYPE.get($ResourceLocation.parse("ytech:aqueduct"))
+
+//     let block = $BuiltInRegistries.BLOCK.get($ResourceLocation.parse("milf:aqueduct_pump_connector"))
+
+//     if (blockEntityType && block) {
+//         blockEntityType.addSupportedBlock(block)
+//     }
+// })

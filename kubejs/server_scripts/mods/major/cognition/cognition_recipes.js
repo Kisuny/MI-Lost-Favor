c4ac8724444d4daab42e2dd5cd4f41456e4b67a5
Kisuny <kitsunyme@gmail.com>
2026-07-31 06:12:51 +0300

    new recipe for cognitive_flux

--- kubejs/server_scripts/mods/major/cognition/cognition_recipes.js
+++ kubejs/server_scripts/mods/major/cognition/cognition_recipes.js
@@ -1,30 +1,19 @@
 ServerEvents.recipes(event => {
     // event.remove({id: "cognition:cognitive_flux"})
     event.replaceInput({ mod: 'cognition' }, 'minecraft:emerald', 'enchanted:attuned_stone')
-    
-    // event.shapeless(Item.of("cognition:cognitive_flux", 4),
-    //     [
-    //         "minecraft:quartz",
-    //         "minecraft:lapis_lazuli",
-    //         [{ "item": "spectrum:topaz_powder" }, { "item": "spectrum:citrine_powder" }, { "item": "spectrum:amethyst_powder" }],
-    //         [{ "item": "minecraft:soul_sand" }, { "item": "minecraft:soul_soil" }],
-    //         "spectrum:shimmerstone_gem"
-    //     ]).id("milf:cognitive_flux");
+    event.replaceInput({ mod: 'cognition' }, 'minecraft:copper_ingot', 'embers:dawnstone_ingot')
 
-
-    alchemyTableCraft(event, {
-        input: [
-            { tag: 'milf:basic_gemstone_powders' },
-            { item: 'minecraft:quartz' },
-            { item: 'minecraft:lapis_lazuli' },
-            { item: 'minecraft:soul_sand' },
-            { item: 'spectrum:shimmerstone_gem' },
+    miMachineRecipe(event, {energy:2, time:100, machine:"modern_industrialization:mixer",
+        inputItems:[
+            [{tag:'milf:basic_gemstone_powders'}, 4],
+            [{item:"minecraft:soul_sand"}, 1, 0.5],
+            [{item:"minecraft:lapis_lazuli"}],
         ],
-        output: 'cognition:cognitive_flux',
-        count: 8,
-        syphon: 50,
-        ticks: 200,
-        upgradeLevel: 2,
+        inputFluids: [
+            [{ fluid: "milf:shimmersteel_essence" }, 20],
+        ],
+        outputItems:[[{item:"cognition:cognitive_flux"}, 6]],
         removeRecipe: true
-    });
+    })
+
 });

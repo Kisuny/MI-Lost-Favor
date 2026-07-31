c74c129932de8c9a646455aca7f27621a26a7d61
Kisuny <kitsunyme@gmail.com>
2026-07-31 06:13:37 +0300

    A ton of new recipes for Malum

--- kubejs/server_scripts/mods/major/malum/malum_recipes.js
+++ kubejs/server_scripts/mods/major/malum/malum_recipes.js
@@ -7,7 +7,8 @@
  *   spirits: Array<{
  *     count: number,
  *     type: 'malum:sacred' | 'malum:wicked' | 'malum:arcane' | 'malum:eldritch' | 'malum:aerial' | 'malum:aqueous' | 'malum:earthen' | 'malum:infernal' | 'malum:umbral'
- *   }>
+ *   }>,
+ *   removeRecipe?: boolean
  * }} args
  */
 const spiritInfusion = (event, args) => {
@@ -18,45 +19,167 @@ const spiritInfusion = (event, args) => {
         extraInputs: args.extraInputs,
         spirits: args.spirits
     })
+    if (args.removeRecipe) { event.remove({ output: args.result.id, type: "malum:spirit_infusion" }) }
 }
 
 ServerEvents.recipes(event => {
 
-    event.remove({ id: "malum:copper_nugget_from_ingot" })
-    event.remove({ id: "malum:copper_ingot_from_nugget" })
+    const removing_by_recipe_id = [
+        "malum:copper_nugget_from_ingot",
+        "malum:copper_ingot_from_nugget",
+        "malum:soulstone_from_deepslate_smelting",
+        "malum:soulstone_from_smelting",
+        "malum:soulstone_from_raw_smelting",
+        "malum:soulstone_from_crushed_smelting",
+        "malum:soulstone_from_deepslate_blasting",
+        "malum:soulstone_from_blasting",
+        "malum:soulstone_from_raw_blasting",
+        "malum:soulstone_from_crushed_blasting",
+        "malum:blazing_quartz_from_smelting",
+        "malum:blazing_quartz_from_blasting",
+    ]
+
+    removing_by_recipe_id.forEach(id => {
+        event.remove({ id: id })
+    });
+
     event.remove({ output: "malum:crude_scythe" })
 
 
-    
+    miMachineRecipe(event, {
+        energy: 2, time: 40, machine: "modern_industrialization:blast_furnace",
+        inputItems: [
+            [{ item: "eidolon_repraised:soul_shard" }, 1],
+            [{ tag: "c:stones" }, 1],
+        ],
+        outputItems: [[{ item: "malum:refined_soulstone" }, 1]]
+    })
+
+    customWorktable(event, {
+        pattern: [
+            " e ",
+            "wqw",
+            "qqq"
+        ],
+        reagents: ["rrrr"],
+        key: {
+            "q": { "item": "malum:runewood_planks" },
+            "w": { "item": "embers:dawnstone_ingot" },
+            "e": { "item": "malum:refined_soulstone" },
+            "r": { "item": "malum:arcane_spirit" },
+        },
+        result: "malum:spirit_altar",
+        removeRecipe: true
+    })
+
     spiritInfusion(event, {
-        input: { item: "spectrum:moonstone_block", count: 1 },
+        input: { item: "milf:fire_clay_ball", count: 4 },
+        result: { id: "malum:alchemical_calx", count: 4 },
+        extraInputs: [],
+        spirits: [
+            { type: "malum:arcane", count: 2 },
+            { type: "malum:earthen", count: 2 },
+            { type: "malum:aqueous", count: 2 }
+        ],
+        removeRecipe: true
+    })
+
+    spiritInfusion(event, {
+        input: { item: "embers:dawnstone_ingot", count: 1 },
+        result: { id: "malum:hallowed_gold_ingot", count: 1 },
         extraInputs: [
-            {
-                item: "modern_industrialization:electronic_circuit",
-                count: 2
-            },
-            {
-                item: "minecraft:barrier",
-                count: 1
-            },
-            {
-                item: "malum:malignant_pewter_plating",
-                count: 4
-            },
-            {
-                item: "malum:fused_consciousness",
-                count: 1
-            },
+            { tag: "c:gems/quartz", count: 4 }
+        ],
+        spirits: [
+            { type: "malum:sacred", count: 2 },
+            { type: "malum:arcane", count: 1 }
+        ],
+        removeRecipe: true
+    })
+
+    spiritInfusion(event, {
+        input: { item: "modern_industrialization:sulfur_dust", count: 1 },
+        result: { id: "malum:hex_ash", count: 1 },
+        extraInputs: [],
+        spirits: [
+            { type: "malum:arcane", count: 1 }
+        ],
+        removeRecipe: true
+    })
+
+    spiritInfusion(event, {
+        input: { item: "neovitae:tainted_flesh", count: 2 },
+        result: { id: "malum:living_flesh", count: 2 },
+        extraInputs: [],
+        spirits: [
+            { type: "malum:sacred", count: 2 },
+            { type: "malum:wicked", count: 2 }
+        ],
+        removeRecipe: true
+    })
+
+    spiritInfusion(event, {
+        input: { item: "modern_industrialization:stainless_steel_ingot", count: 2 },
+        result: { id: "malum:malignant_pewter_ingot", count: 1 },
+        extraInputs: [
+            { item: "malum:malignant_lead", count: 1 },
+            { item: "malum:null_slate", count: 8 },
+            { item: "minecraft:netherite_scrap", count: 2 }
         ],
         spirits: [
             { type: "malum:earthen", count: 16 },
-            { type: "malum:wicked", count: 8 },
-            { type: "malum:arcane", count: 22 },
-            { type: "malum:umbral", count: 1 },
+            { type: "malum:eldritch", count: 16 }
         ],
-        result: {
-            id: "milf:moonstone_table_core",
-            count: 1
-        }
+        removeRecipe: true
     })
+
+    spiritInfusion(event, {
+        input: { item: "modern_industrialization:steel_ingot", count: 1 },
+        result: { id: "malum:soul_stained_steel_ingot", count: 1 },
+        extraInputs: [
+            { item: "malum:refined_soulstone", count: 4 }
+        ],
+        spirits: [
+            { type: "malum:wicked", count: 3 },
+            { type: "malum:earthen", count: 1 },
+            { type: "malum:arcane", count: 1 }
+        ],
+        removeRecipe: true
+    })
+
+    spiritInfusion(event, {
+        input: { item: "hexerei:infused_fabric", count: 2 },
+        result: { id: "malum:soulwoven_silk", count: 4 },
+        extraInputs: [
+            { tag: "c:strings", count: 2 }
+        ],
+        spirits: [
+            { type: "malum:aerial", count: 3 },
+            { type: "malum:earthen", count: 3 }
+        ],
+        removeRecipe: true
+    })
+
+    spiritInfusion(event, {
+        input: { item: "ars_nouveau:sourcestone", count: 16 },
+        result: { id: "malum:tainted_rock", count: 16 },
+        extraInputs: [],
+        spirits: [
+            { type: "malum:sacred", count: 1 },
+            { type: "malum:arcane", count: 1 }
+        ],
+        removeRecipe: true
+    })
+
+    spiritInfusion(event, {
+        input: { item: "ars_nouveau:sourcestone", count: 16 },
+        result: { id: "malum:twisted_rock", count: 16 },
+        extraInputs: [],
+        spirits: [
+            { type: "malum:wicked", count: 1 },
+            { type: "malum:arcane", count: 1 }
+        ],
+        removeRecipe: true
+    })
+
 })

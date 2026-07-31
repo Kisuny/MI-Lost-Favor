dacbe87be83ad62deee022ef52e8f858bacabcb4
Kisuny <kitsunyme@gmail.com>
2026-07-31 09:41:15 +0300

    add structure_block_replace.js to swap early-game-inaccessible blocks in generated structures

---
+++ kubejs/server_scripts/structure_block_replace.js
@@ -0,0 +1,34 @@
+const BLOCK_REPLACEMENTS = {
+    'minecraft:enchanting_table': 'minecraft:lectern',
+    'minecraft:blast_furnace': 'ytech:primitive_smelter',
+    'minecraft:crafting_table': 'minecraft:bookshelf',
+    'minecraft:hopper': 'woodenhopper:wooden_hopper',
+    'immersiveengineering:workbench': 'supplementaries:clock_block',
+    'immersiveengineering:crate': 'minecraft:barrel',
+    'eidolon_repraised:lead_block': 'modern_industrialization:lead_block',
+    'minecraft:iron_block': 'minecraft:raw_iron_block',
+    'minecraft:gold_block': 'minecraft:raw_gold_block',
+    'eidolon_repraised:crucible': 'minecraft:cauldron',
+};
+
+MoreJS.structureLoad((event) => {
+    event.forEachPalettes((palette) => {
+        palette.forEach((blockInfo) => {
+            const replacement = BLOCK_REPLACEMENTS[blockInfo.id];
+            if (replacement) {
+                console.log(`[milf] ${event.id}: ${blockInfo.id} -> ${replacement}`);
+                blockInfo.setBlock(replacement);
+                return;
+            }
+
+            if (blockInfo.id === 'minecraft:jigsaw') {
+                const finalState = blockInfo.getNbt().getString('final_state');
+                const jigsawReplacement = BLOCK_REPLACEMENTS[finalState];
+                if (jigsawReplacement) {
+                    console.log(`[milf] ${event.id}: jigsaw final_state ${finalState} -> ${jigsawReplacement}`);
+                    blockInfo.nbt().putString('final_state', jigsawReplacement);
+                }
+            }
+        });
+    });
+});

42c12159e3f3a57f056827aadc8e1792d49ca9f2
Kisuny <kitsunyme@gmail.com>
2026-08-01 10:59:55 +0300

    added table for alakarkinos_conversion

---
+++ kubejs/server_scripts/loot_tables/custom_loot_tables.js
@@ -0,0 +1,17 @@
+LootJS.lootTables(event => {
+    event
+        .create("milf:archaeology/magic")
+        .createPool(pool => {
+            pool.addEntry(LootEntry.of("malum:raw_brilliance").withWeight(80))
+            pool.addEntry(LootEntry.of("malum:cthonic_gold").withWeight(80))
+            pool.addEntry(LootEntry.of("malum:cthonic_gold_fragment").withWeight(100))
+            pool.addEntry(LootEntry.of("malum:tainted_rock").withWeight(100))
+            pool.addEntry(LootEntry.of("malum:twisted_rock").withWeight(100))
+            pool.addEntry(LootEntry.of("eidolon_repraised:soul_shard").withWeight(60))
+            pool.addEntry(LootEntry.of("spectrum:shimmerstone_gem").withWeight(100))
+            pool.addEntry(LootEntry.of("spectrum:onyx_powder").withWeight(50))
+        })
+
+
+
+})

713673d93e7f170ce2274a8ee6958e49bcbd97c7
saeta <saeta_@outlook.com>
2026-08-03 22:31:40 +0700

    ambient entities annihilator 2000 (limited MILF edition)

---
+++ kubejs/server_scripts/mobs_spawn_cap.js
@@ -0,0 +1,27 @@
+let $FinalizeSpawnEvent = Java.loadClass("net.neoforged.neoforge.event.entity.living.FinalizeSpawnEvent")
+
+NativeEvents.onEvent($FinalizeSpawnEvent, event => {
+    let level = event.getLevel()
+    if (level.isClientSide()) return
+        
+    if (!event.getEntity().ambientCreature) return
+
+    let chunkPos = new $ChunkPos(event.getEntity().getBlock().pos)
+
+    let aabb = AABB.of(
+        chunkPos.getMinBlockX(), level.getMinBuildHeight(), chunkPos.getMinBlockZ(),
+        chunkPos.getMaxBlockX(), level.getMaxBuildHeight(), chunkPos.getMaxBlockZ()
+    )
+
+    let ambientEntitiesInChunk = level["getEntities(net.minecraft.world.entity.Entity,net.minecraft.world.phys.AABB,java.util.function.Predicate)"](
+        null, 
+        aabb, 
+        entity => entity.ambientCreature
+    ).size()
+
+    if (ambientEntitiesInChunk > 4){
+        event.setSpawnCancelled(true)
+        event.setCanceled(true)
+    }
+    
+})

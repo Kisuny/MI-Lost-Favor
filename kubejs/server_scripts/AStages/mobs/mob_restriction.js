189af06357ec3d8b696d46692651849b73146cf2
Kisuny <kitsunyme@gmail.com>
2026-08-08 22:55:59 +0300

    fix astages mob restriction for future version

--- kubejs/server_scripts/AStages/mobs/mob_restriction.js
+++ kubejs/server_scripts/AStages/mobs/mob_restriction.js
@@ -1,7 +1,6 @@
 const mobRestriction = (modId, mobArray) => {
     mobArray.forEach(mob => {
         AStages.addRestrictionForMob(`${modId}/${mob.id}`, `${modId}_mobs`, mob.mob)
-            .set(Attributes.MOB_SPAWNING, true)
             .restrictSpawnType("natural", "chunk_generation", "breeding", "mob_summoned", "jockey", "event", "conversion", "reinforcement", "triggered", "command", "spawn_egg", "dispenser", "patrol", "bucket")
     });
 };
